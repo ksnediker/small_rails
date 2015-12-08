35adc4e8fbdfefe07bb98c2a2a8a0eec7ce22488
Katherine Snediker <KatieSnediker@Katherines-MacBook-Pro.local>
2015-12-07 20:44:07 -0500

    added new, create and index routes; added show and index views

--- to_do_app/app/controllers/tasks_controller.rb
+++ to_do_app/app/controllers/tasks_controller.rb
@@ -1,15 +1,26 @@
 class TasksController < ApplicationController
+	before_action :find_task, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@tasks = Task.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@task = Task.new
 	end
 
 	def create
+		@task = Task.new(task_params)
+
+		if @task.save
+			redirect_to @task
+		else
+			render 'new'
+		end
+		
 	end
 
 	def edit
@@ -24,9 +35,11 @@ class TasksController < ApplicationController
 	private
 
 	def find_task
+		@task = Task.find(params[:id])
 	end
 
 	def task_params
+		params.require(:task).permit(:title, :content)
 	end
 
 end
